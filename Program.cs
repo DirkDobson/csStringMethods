143d89a9cbf5df25b06e30cb2ecca2f82eee602d
Dirk Dobson <dirkvancouver@live.com>
2018-11-29 20:36:22 -0700

    sting methods

--- Program.cs
+++ Program.cs
@@ -15,6 +15,23 @@ namespace StringMethods
             var lastName = fullName.Substring(index + 1);
             System.Console.WriteLine(firstName);
             System.Console.WriteLine(lastName);
+
+            var names = fullName.Split(' ');
+            System.Console.WriteLine("FirstName: " + names[0]);
+            System.Console.WriteLine("LastName: " + names[1]);
+
+            Console.WriteLine(fullName.Replace("Dirk", "Joe"));
+
+            if ( String.IsNullOrWhiteSpace(" "))
+                System.Console.WriteLine("Invalid");
+
+            var str = "25";
+            var age = Convert.ToByte(str);
+            System.Console.WriteLine(age);
+
+            float price = 29.95f;
+            Console.WriteLine(price.ToString("C"));
+            
         }
     }
 }
