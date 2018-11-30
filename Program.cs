8d850072b8512f0f672416646ad5cd3511bbb21a
Dirk Dobson <dirkvancouver@live.com>
2018-11-29 20:24:10 -0700

    inial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StringMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fullName = "Dirk Dobson  ";
+            System.Console.WriteLine("Trim: '{0}'", fullName.Trim());
+            System.Console.WriteLine("ToUpper: '{0}'", fullName.Trim().ToUpper());
+
+            var index = fullName.IndexOf(' ');
+            var firstName = fullName.Substring(0, index);
+            var lastName = fullName.Substring(index + 1);
+            System.Console.WriteLine(firstName);
+            System.Console.WriteLine(lastName);
+        }
+    }
+}
